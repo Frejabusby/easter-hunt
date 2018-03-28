7073d818abdbd386465614f99d10aaacfb76c71c
frejabusby <frejabusby@gmail.com>
2018-03-28 10:16:35 +0200

    Scroll up the page and no scroll on pop up

--- record-union/code/script.js
+++ record-union/code/script.js
@@ -1,18 +1,20 @@
 // A function that adds and remove the class "active" on the section you click on.
 const toogle = () => {
+  document.documentElement.scrollTop = 0;
   const easterEgg = document.getElementById("easter-egg")
   easterEgg.classList.toggle("active")
+  window.addEventListener('scroll', noscroll);
 }
 
-const toogle1 = () => {
-  const easterEgg = document.getElementById("easter-egg")
-  easterEgg.classList.toggle("active-66")
-  console.log("toogle1")
-}
+// const toogle1 = () => {
+//   const easterEgg = document.getElementById("easter-egg")
+//   easterEgg.classList.toggle("active-66")
+// }
 
 const closePopUp = () => {
   const easterEgg = document.getElementById("easter-egg")
   easterEgg.classList.remove("active")
+  window.removeEventListener('scroll', noscroll);
 }
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
@@ -43,6 +45,10 @@ const random = (a) => {
   randomNumber.slice(0, 11).forEach((num) => {
     document.getElementById(`hide-egg-${num}`).onclick = toogle
   })
+}
+
+const noscroll = () => {
+  window.scrollTo(0, 0)
 }
 
 document.getElementById("close-pop-up").onclick = closePopUp
