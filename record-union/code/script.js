b6d80ac7edd54540e6217653d8b11e71fa0e4689
KarinFF <karinfrostell@gmail.com>
2018-03-28 17:27:58 +0200

    cleaning up our code

--- record-union/code/script.js
+++ record-union/code/script.js
@@ -14,7 +14,7 @@ const closePopUp = () => {
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
 const shuffle = (a) => {
-    for(let i = a.length - 1; i > 0; i--) {
+  for(let i = a.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [a[i], a[j]] = [a[j], a[i]];
   } return a
@@ -23,7 +23,7 @@ const shuffle = (a) => {
 const random = () => {
   const randomNumber = shuffle(numbers)
   randomNumber.slice(0, 12).forEach((num) => {
-  document.getElementById(`hide-egg-${num}`).onclick = toogle
+    document.getElementById(`hide-egg-${num}`).onclick = toogle
   })
 }
 
